c314c47098dad73a7f0cd6778143e5d86387570b
NgBlaze <okolaaprecious@gmail.com>
2024-07-09 11:05:26 +0100

    Corrected route path

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('users/{id}', [OrganisationController::class, 'getUser']);
-    Route::get('organisations', [OrganisationController::class, 'getOrganisations']);
-    Route::get('organisations/{orgId}', [OrganisationController::class, 'getOrganisation']);
-    Route::post('organisations', [OrganisationController::class, 'createOrganisation']);
-    Route::post('organisations/{orgId}/users', [OrganisationController::class, 'addUserToOrganisation']);
+    Route::get('api/users/{id}', [OrganisationController::class, 'getUser']);
+    Route::get('api/organisations', [OrganisationController::class, 'getOrganisations']);
+    Route::get('api/organisations/{orgId}', [OrganisationController::class, 'getOrganisation']);
+    Route::post('api/organisations', [OrganisationController::class, 'createOrganisation']);
+    Route::post('api/organisations/{orgId}/users', [OrganisationController::class, 'addUserToOrganisation']);
 });
